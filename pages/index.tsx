dd55b99a7a268cfef318ee74d0f741915b40994f
André Luiz <andre.luiz.a.f@hotmail.com>
2023-06-21 16:59:45 -0300

    Criação dos elementos, do visual e também da sua responsividade - Seção de cards, homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import HeaderNoAuth from '../src/components/homeNoAuth/headerNoAuth';
 import styles from '../styles/homeNoAuth.module.scss';
 import PresentationSection from '@/src/components/homeNoAuth/presentationSection';
+import CardsSection from '@/src/components/homeNoAuth/cardsSection';
 
 const HomeNotAuth = function () {
   return (
@@ -19,6 +20,7 @@ const HomeNotAuth = function () {
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <CardsSection />
       </main>
     </>
   );

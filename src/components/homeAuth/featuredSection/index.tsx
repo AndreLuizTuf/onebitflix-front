2058fdc1dea63c6f7d3187a5386aa800a38d3cb1
André Luiz <andre.luiz.a.f@hotmail.com>
2023-06-23 16:35:08 -0300

    Criação da página home autenticada, do seu estilo visual e também da sua responsividade completa. Implementação de todas as categorias na página e criação de seriços - Home autenticada

--- src/components/homeAuth/featuredSection/index.tsx
+++ src/components/homeAuth/featuredSection/index.tsx
@@ -18,7 +18,7 @@ const FeaturedSection = function () {
   return (
     <>
       {
-        data.data.map((course: CourseType) => (
+        data.data?.map((course: CourseType) => (
           <div
             style={{
               backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl})`,
@@ -28,7 +28,7 @@ const FeaturedSection = function () {
             }}
             key={course.id}>
             <HeaderAuth />
-            <Container>
+            <Container className="pt-4">
               <p className={styles.title}>{course.name}</p>
               <p className={styles.description}>{course.synopsis}</p>
               <Link href={`/courses/${course.id}`}>
